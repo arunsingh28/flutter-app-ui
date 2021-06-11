58618f1660bde0445f21a9b10886684ab573b8a6
Arun Singh <arunsingh28aug.as@gmail.com>
2021-06-11 23:51:40 +0530

    sperate home_page

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: buildAppBar(),
+      drawer: Drawer(
+        child: Container(
+          child: Text('Menu'),
+        )
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      title: Text("Help on"),
+      backgroundColor: Colors.orangeAccent, 
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,18 +7,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.light(),
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Arun'),
-          ),
-        ),
-      )
+      home: HomePage()
     );
   }
 }
