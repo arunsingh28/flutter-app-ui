e7af5bb4a440f66d1bb4bd30a1ab299ec3476f70
Arun Singh <arunsingh28aug.as@gmail.com>
2021-06-13 01:26:15 +0530

    day off

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,10 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
+      theme: ThemeData(
+          fontFamily: GoogleFonts.lato().fontFamily,
+        ),
       routes: {
-        "/" : (context) => LoginPage(),
-        "/login" : (context) => HomePage(),
+        "/": (context) => LoginPage(),
+        "/login": (context) => HomePage(),
       },
     );
   }
