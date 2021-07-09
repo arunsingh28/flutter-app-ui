598c19305725921777215942b5bd80be6112f994
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-09 12:26:28 +0530

    windows commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -25,3 +24,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
