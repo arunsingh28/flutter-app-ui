c012562078f559dd26ab0448474cddc76a26902b
Arun Singh <arunsingh28aug.as@gmail.com>
2021-08-18 00:17:18 +0530

    listview

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:learning_app/models/catalog.dart';
 import 'package:learning_app/widget/drawer.dart';
+import 'package:learning_app/widget/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -7,14 +9,14 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('App bar'),
       ),
-      drawer:MyDrawer(),
-      body: Column(
-        children:[ 
-          Container(
-          child: Text('asd'),
-          ),
-          
-        ]
+      drawer: MyDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ListView.builder(
+            itemCount: Catalog.item.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: Catalog.item[index]);
+            }),
       ),
     );
   }
